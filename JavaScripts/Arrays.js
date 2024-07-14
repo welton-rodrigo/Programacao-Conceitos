7e62c8c3565e41129a623182ecc55f43bd6d0b7d
weltontech <weltonjc1@hotmail.com>
2024-07-13 23:45:47 -0300

    JavaScript

--- JavaScripts/Arrays.js
+++ JavaScripts/Arrays.js
@@ -5,3 +5,13 @@ var Data = {
 }
 
 console.log(Data)
+
+/**funçoes é um bloco de código javaScritp nomeado e parametrizado
+que voce define uma vez, e então pode chamar repetidas vezes**/
+function somaNumero(x){
+    return x + 1;
+}
+//Interessante funções são valores e podem ser atribuidas a variaveis
+var square = function somaTest(d){
+    return d + 2;
+};
