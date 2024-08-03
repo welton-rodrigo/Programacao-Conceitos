6d64b6ded99f708e9532195c199f55c2ccbc6543
weltontech <weltonjc1@hotmail.com>
2024-08-02 22:32:41 -0300

    gerenciando dados de input _ e.target.value

---
+++ ReactWorkspace/5_FORM_EM_REACT/form/src/App.js
@@ -0,0 +1,15 @@
+
+import logo from './logo.svg';
+import './App.css';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Form</h1>
+     <MyForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactWorkspace/5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -0,0 +1,37 @@
+import './MyForm.module.css';
+import {useState} from 'react';
+const MyForm = () => {
+  //3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+console.log(name);
+  return (
+    <div>
+        {/*1 - criacao de form*/}
+        <form>
+            <div>
+            <label htmlFor="name">Nome:</label>
+            <input
+             type="text" 
+             name="name" 
+             placeholder="digite seu nome"
+             onChange={handleName}
+             />
+            </div>
+            {/*Label envolvendo input*/}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="digite seu email" />
+            </label>
+
+                <input type="submit" value="enviar" />
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
