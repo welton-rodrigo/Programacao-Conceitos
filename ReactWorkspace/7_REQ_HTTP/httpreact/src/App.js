9806f718c3be61e48dde8b8695487627f742c803
weltontech <weltonjc1@hotmail.com>
2024-08-12 17:09:05 -0300

    salvando dados

--- ReactWorkspace/7_REQ_HTTP/httpreact/src/App.js
+++ ReactWorkspace/7_REQ_HTTP/httpreact/src/App.js
@@ -7,6 +7,8 @@ const url = "http://localhost:3000/products"
 function App() {
   const [products, setProducts] = useState([])
   
+  const [name, setName] = useState([]);
+  const [price, setPrice] = useState([]);
   
   //1 - resgatando dados
   useEffect(() =>{
@@ -24,6 +26,25 @@ function App() {
   }, []);
 
 
+  // 2 add produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault() //para nao submeter formulario
+
+    const product = {
+      name,  //quando o vamor tem o mesmo nome da chave não precisamos colocar name: name 
+      price, 
+    };
+    const res  = await fetch("http://localhost:3000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type:" : "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+
+  };
+
 
   return (
     <div className="App">
@@ -33,6 +54,21 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+
+        <div className='add-product'>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Nome:
+              <input type="text" value={name} name='name' onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+              Preço:
+              <input type="text" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+            </label>
+            <input type="submit" value="Criar" />
+          </form>
+        </div>
+
     </div>
   );
 }
