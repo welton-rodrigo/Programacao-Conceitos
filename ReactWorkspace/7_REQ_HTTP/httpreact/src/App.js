9b154f57c5b4890216cfcf412554b25f33559a9f
weltontech <weltonjc1@hotmail.com>
2024-08-23 14:29:17 -0300

    function useFetch

--- ReactWorkspace/7_REQ_HTTP/httpreact/src/App.js
+++ ReactWorkspace/7_REQ_HTTP/httpreact/src/App.js
@@ -1,31 +1,45 @@
 import './App.css';
 
 import {useState, useEffect} from "react";
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
 
 const url = "http://localhost:3000/products"
 
 function App() {
   //locais pra salvar os dados
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
+
+  //5 - refatorando post
+  const [config, setConfig] = useState(null)
+  const [method, setMethod] = useState(null)
+  const [callFetch, setCallFetch] = useState(false)
+  
+
+
+  //4 -custom
+  const { data: items } = useFetch(url);
+
+
   const [name, setName] = useState([]);
   const [price, setPrice] = useState([]);
   
   //1 - resgatando dados
   //chamada asyncrona 
-  useEffect(() =>{
+ // useEffect(() =>{
     
-    async function fetchData(){
+   // async function fetchData(){
     
-    const res = await fetch(url)
+   // const res = await fetch(url)
 
     //transformando em json object
-    const data = await res.json()
+    //const data = await res.json()
 
-    setProducts(data)
-  }
+    //setProducts(data)
+  //}
   
-  fetchData();
-  }, []);
+  //fetchData();
+  //}, []);
 
 
   // 2 add produtos
@@ -36,13 +50,26 @@ function App() {
       name,  //quando o vamor tem o mesmo nome da chave não precisamos colocar name: name 
       price, 
     };
+
+    console.log(product);
     const res  = await fetch("http://localhost:3000/products", {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type:" : "application/json",
+        "Content-Type" : "application/Json",
       },
       body: JSON.stringify(product),
     });
+
+    // 3 - carregamento dinamico
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    //limpeza dos inputs
+    setName("")
+    setPrice("")
+
+
 
 
   };
@@ -52,7 +79,7 @@ function App() {
     <div className="App">
         <h1>Lista de Produtos</h1>
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
@@ -65,7 +92,7 @@ function App() {
             </label>
             <label>
               Preço:
-              <input type="text" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
+              <input type="number" value={price} name='price' onChange={(e) => setPrice(e.target.value)} />
             </label>
             <input type="submit" value="Criar" />
           </form>
